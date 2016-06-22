a0a911fb7905d1745be59b6f992b350e6aec228a
cstk235 <nguyenchausondl@gmail.com>
2016-06-22 17:38:16 +0700

    Update codechongcopyHaDo.js

--- codechongcopyHaDo.js
+++ codechongcopyHaDo.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://datanla.net/'>http://datanla.net/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Click vào đây để lấy bài : <a href='http://www.canho-centrosagarden.com/2016/04/can-ho-centrosa-garden-gia-goc-tu-chu-dau-tu.html'+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
