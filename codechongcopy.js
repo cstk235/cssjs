f0b4093a4ddc29f321dc19d4129bf1ced2b03b56
cstk235 <nguyenchausondl@gmail.com>
2016-07-12 16:14:06 +0700

    Update codechongcopy.js

--- codechongcopy.js
+++ codechongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/> Click vào đây để lấy nội dung bài viết : <a href='http://datanla.net/'>http://datanla.net/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> Click vào đây để lấy nội dung bài viết : <a href='http:http://eagleland.vn/du-an/ha-do-centrosa-garden'>http://eagleland.vn/du-an/ha-do-centrosa-garden"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
