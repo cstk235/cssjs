7819c17bf499237846167f75681e06c2645b61c6
cstk235 <nguyenchausondl@gmail.com>
2017-03-21 13:58:50 +0700

    Update codechongcopy.js

--- codechongcopy.js
+++ codechongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/> Truy cập link sau để lấy nội dung bài viết : <a href='http://eagleland.vn'>http://eagleland.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> Truy cập link sau để lấy nội dung bài viết : <a href='http://datanla.net'>http://datanla.net"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
