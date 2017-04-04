a3cdd0181560e2909abfb0b4b7cc6ab275515a50
cstk235 <nguyenchausondl@gmail.com>
2017-04-04 17:27:20 +0700

    Update codechongcopyHaDo.js

--- codechongcopyHaDo.js
+++ codechongcopyHaDo.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://valoraisland.net/'>Valora Island"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Truy cập link sau kéo xuống dưới cùng để lấy nội dung bài viết : <a href='http://valoraisland.net/'>Valora Island"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
