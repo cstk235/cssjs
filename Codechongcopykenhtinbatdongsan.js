69becd4b8ffd21eb669cf557c740842df52f2c65
cstk235 <nguyenchausondl@gmail.com>
2017-05-04 16:27:21 +0700

    Update Codechongcopykenhtinbatdongsan.js

--- Codechongcopykenhtinbatdongsan.js
+++ Codechongcopykenhtinbatdongsan.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/> Click vào link sau kéo xuống dưới cùng có file bài viết để copy: <a href='http://kenhtinbatdongsan.com.vn/'>http://kenhtinbatdongsan.com.vn/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> Click vào link sau kéo xuống dưới cùng có file bài viết để copy: <a href='http://kenhtinbatdongsan.com.vn'>http://kenhtinbatdongsan.com.vn"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
